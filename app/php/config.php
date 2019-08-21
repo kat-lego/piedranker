e8165030dfbf29f5f146444a53c95c9820cefb36
root <root@moodle.ms.wits.ac.za>
2019-08-21 14:39:33 +0200

    Removed config.php

--- app/php/config.php
+++ app/php/config.php
@@ -1,18 +1,10 @@
 <?php
 
 // MYSQL configuration
-if($_SERVER['SSH_CLIENT'] == '10.10.187.45 47110 22'){
-    define('DB_NAME', 'moodle');
-    define('DB_USERNAME', 'username');
-    define('DB_PASSWORD', 'password');
-    define('DB_HOST', '127.0.0.1');
-    define('TABLE_PREFIX','mdl_');
-}else{
     define('DB_NAME', 'moodle');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
     define('DB_HOST', '127.0.0.1');
     define('TABLE_PREFIX','mdl_');
-}
 
 ?>
